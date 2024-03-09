7ed30fa015c48d84c8eb401c872376ec86cde4fd
qdave5 <qosimd@gmail.com>
2024-03-09 11:15:55 +0700

    add Camera move limit

--- scripts/worlds/MainLevel.gd
+++ scripts/worlds/MainLevel.gd
@@ -10,6 +10,7 @@ onready var mainTheme : Dictionary = get_node("/root/Global").MAIN_THEME
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	setLevelColor()
+	setCameraBound()
 
  
 func setLevelColor():
@@ -18,6 +19,13 @@ func setLevelColor():
 	$YSort/WallBGTileMap.modulate = mainTheme['summer'].level.wall
 	$YSort/Door.modulate = mainTheme['summer'].level.door
 	$YSort/Desk.modulate = mainTheme['summer'].level.desk
+
+func setCameraBound():
+	camera.limit_left = - 100
+	camera.limit_top = - 100 - 128
+	camera.limit_bottom = floorTexture.rect_size.y + 100
+	camera.limit_right = floorTexture.rect_size.x + 100
+
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
